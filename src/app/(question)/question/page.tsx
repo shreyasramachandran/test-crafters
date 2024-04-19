1dbaa23625ba8a2e0de1ab85b2434a8c1e8bf3ae
shreyas <shreyasramachandran@gmail.com>
2024-04-19 10:25:27 +0530

    half way through development of test navigation and management system

--- src/app/(question)/question/page.tsx
+++ src/app/(question)/question/page.tsx
@@ -1,7 +1,16 @@
+"use client"
 import { Flex, Box, Text, Button, RadioGroup } from "@radix-ui/themes";
+import { useState } from 'react';
 
 export default function Page() {
+    const [questionNumber, setquestionNumber] = useState(1);
+    const question = 'Find out one substitution for the following phrase from the options given below "Never done or known before".'
     const options = ['Unprecedented', 'Unpronounceable', 'Unprotected', 'Unquiet']
+
+    const incrementQuestionNumber = (incrementCount: number) => {
+        setquestionNumber(questionNumber + incrementCount);
+    };
+
     return (
         <Flex className="bg-[#F6F7FB]" direction='column' height={{ md: '100vh' }} width={{ md: '100vw' }}>
             <Box className="bg-[#0144FF] bg-opacity-[0.059]" flexGrow='1' style={{ height: '5%', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
@@ -12,7 +21,7 @@ export default function Page() {
                     <Box style={{ 'height': '100%', 'width': '70%' }}>
                         <Flex direction='column' style={{ height: '100%', width: '100%' }} gap='4'>
                             <Box style={{ 'height': '15%', 'width': '100%', 'display': 'flex', 'alignItems': 'center' }}>
-                                <Text className="pl-8" size='6' weight='bold' wrap='pretty' >Question 1</Text>
+                                <Text className="pl-8" size='6' weight='bold' wrap='pretty' >Question {questionNumber}</Text>
                             </Box>
                             <Box style={{ 'height': '5%', 'width': '100%', 'display': 'flex', 'alignItems': 'center' }}>
                                 <div className="flex items-center justify-center w-full p-8">
@@ -20,8 +29,7 @@ export default function Page() {
                                 </div>
                             </Box>
                             <Box style={{ 'height': '10%', 'width': '100%', 'display': 'flex', 'alignItems': 'center' }}>
-                                <Text className="pl-8" size='4' weight='light' wrap='pretty' >Find out one substitution for the following phrase from the options given below
-                                    &ldquo;Never done or known before&rdquo;.</Text>
+                                <Text className="pl-8" size='4' weight='light' wrap='pretty' >{question}</Text>
                             </Box>
                             <Box style={{ 'height': '20%', 'width': '100%', 'display': 'flex', 'alignItems': 'center' }}>
                                 <RadioGroup.Root size='3' name="Options" className="pl-8" style={{ fontSize: '1.1rem', width: '100%' }}>
@@ -38,10 +46,10 @@ export default function Page() {
                                 </div>
                             </Box>
                             <Box className="gap-8" style={{ 'height': '10%', 'width': '100%', 'display': 'flex', 'alignItems': 'center', 'justifyContent': 'center' }}>
-                                <Button style={{ borderRadius: '5px' }} size="3" variant='solid'>Save and Next</Button>
-                                <Button style={{ borderRadius: '5px' }} size="3" variant='solid'>Clear Response</Button>
-                                <Button style={{ borderRadius: '5px' }} size="3" variant='solid'>Save and Mark for Review</Button>
-                                <Button style={{ borderRadius: '5px' }} size="3" variant='solid'>Mark for Review and Next</Button>
+                                <Button style={{ borderRadius: '5px' }} size="3" variant='solid' onClick={() => incrementQuestionNumber(1)}>Save and Next</Button>
+                                <Button style={{ borderRadius: '5px' }} size="3" variant='solid' onClick={() => incrementQuestionNumber(1)}>Clear Response</Button>
+                                <Button style={{ borderRadius: '5px' }} size="3" variant='solid' onClick={() => incrementQuestionNumber(1)}>Save and Mark for Review</Button>
+                                <Button style={{ borderRadius: '5px' }} size="3" variant='solid' onClick={() => incrementQuestionNumber(1)}>Mark for Review and Next</Button>
                             </Box>
                         </Flex>
                     </Box>
@@ -53,12 +61,12 @@ export default function Page() {
                             <Box className="pl-8" style={{ 'height': '50%', 'width': '100%', 'display': 'grid', 'gridTemplateColumns': 'repeat(8, 1fr)', 'alignItems': 'center', 'justifyItems': 'center' }}>
                                 {Array.from({ length: 50 }).map((_, index) => (
                                     <div key={index} className="flex flex-col items-center justify-center"> {/* Add key prop here */}
-                                        <div className="w-8 h-8 flex items-center justify-center rounded-lg text-white bg-gradient-to-tr from-stone-500 to-stone-100 text-sm">
+                                        <button
+                                            className="w-8 h-8 flex items-center justify-center rounded-lg text-white bg-gradient-to-tr from-stone-500 to-stone-100 text-sm">
                                             {index + 1}
-                                        </div>
+                                        </button>
                                     </div>
                                 ))}
-
                             </Box>
                             <Box style={{ 'height': '7%', 'width': '90%', 'display': 'flex', 'alignItems': 'center' }}>
                                 <Text className="pl-8" size='4' weight='bold' wrap='pretty' >Legend</Text>
