70c7c660a924ab749de4b7a72ab353f9e1226d40
Shreyas Ramachandran <shreyasramachandran@gmail.com>
2024-05-02 10:33:25 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/your-day",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-export default nextConfig;
+module.exports = nextConfig;
