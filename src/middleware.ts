23c2afea2ed6f2f65812fcf83b05b05671588e90
shreyas <shreyasramachandran@gmail.com>
2024-05-08 12:44:08 +0530

    added new subjects

--- src/middleware.ts
+++ src/middleware.ts
@@ -42,7 +42,7 @@ export async function middleware(request: NextRequest) {
             name: 'sessionId',
             value: sessionId, // Generate a random session IDx
             path: '/',
-            domain: '.cuet.net.in',
+            domain: 'localhost',
             httpOnly: true,
             secure: true, // Set to true if using HTTPS
             sameSite: 'none', // Recommended for most use cases
