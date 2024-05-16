b65b80aab82536bd8fc5b24969f0a09aeda1f343
shreyas <shreyasramachandran@gmail.com>
2024-05-16 18:25:30 +0530

    added anthropology

--- src/app/(test-picker)/test-picker/page.tsx
+++ src/app/(test-picker)/test-picker/page.tsx
@@ -31,7 +31,8 @@ const MainComponent = () => {
     const isAuthenticated = useAuth();
     // These need to be populated from metadata
     const subjects = ['Political Science', 'English', 'French', 'Agriculture', 'Chemistry',
-        'General Test', 'Physics', 'Sociology', 'Computer Science', 'Home Science', 'Economics', 'Legal Studies', 'Fine Arts']
+        'General Test', 'Physics', 'Sociology', 'Computer Science', 'Home Science', 'Economics', 'Legal Studies', 'Fine Arts',
+        'Anthropolgy']
     const languages = ['English']
 
     const [selectedSubject, setSelectedSubject] = useState("Subject");
