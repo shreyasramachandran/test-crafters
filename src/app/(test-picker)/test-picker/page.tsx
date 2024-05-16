fca7d2412f8f71d8391bb3ad09ad0204755dcc19
shreyas <shreyasramachandran@gmail.com>
2024-05-16 16:50:13 +0530

    added new subjects

--- src/app/(test-picker)/test-picker/page.tsx
+++ src/app/(test-picker)/test-picker/page.tsx
@@ -31,7 +31,7 @@ const MainComponent = () => {
     const isAuthenticated = useAuth();
     // These need to be populated from metadata
     const subjects = ['Political Science', 'English', 'French', 'Agriculture', 'Chemistry',
-        'General Test', 'Physics', 'Sociology', 'Computer Science', 'Home Science', 'Economics']
+        'General Test', 'Physics', 'Sociology', 'Computer Science', 'Home Science', 'Economics', 'Legal Studies', 'Fine Arts']
     const languages = ['English']
 
     const [selectedSubject, setSelectedSubject] = useState("Subject");
