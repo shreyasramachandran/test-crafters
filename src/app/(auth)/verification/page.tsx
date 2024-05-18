602fa16f7ba83c97d3059ac62ce2239ca6d3d176
shreyas <shreyasramachandran@gmail.com>
2024-05-18 09:12:41 +0530

    changed auth design

--- src/app/(auth)/verification/page.tsx
+++ src/app/(auth)/verification/page.tsx
@@ -68,8 +68,8 @@ export default function Page() {
     };
 
     return (
-        <Flex className="bg-[#F6F7FB]" height={{ md: '100vh' }} width={{ md: '100vw' }} justify='center' align='center'>
-            <Box className="bg-white" style={{ 'height': '55%', 'width': '30%', 'borderRadius': '5px', 'boxShadow': '4px 4px 50px 5px rgba(0, 0, 0, 0.25)' }}>
+        <Flex className="bg-[#38B6FF]" height={{ md: '100vh' }} width={{ md: '100vw' }} justify='center' align='center'>
+            <Box className="bg-[#EAF6FA]" style={{ 'height': '55%', 'width': '30%', 'borderRadius': '5px', 'boxShadow': '4px 4px 50px 5px rgba(0, 0, 0, 0.25)' }}>
                 <Flex className="h-full" direction='column' justify='start' gap='5' pt='7'>
                     <Box style={{ 'height': '15%', 'width': '50%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                         <Text size='6' weight='bold' wrap='pretty'>Verification</Text>
@@ -80,14 +80,14 @@ export default function Page() {
                     <Box style={{ 'height': '15%', 'width': '77%', display: 'flex', justifyContent: 'end', alignItems: 'center', alignSelf: 'center' }}>
                         <input
                             type="text"
-                            className="border border-solid border-[#79747E] bg-white h-10 px-5  text-sm focus:outline-none"
+                            className="border border-solid border-[#79747E] bg-[#EAF6FA] h-10 px-5  text-sm focus:outline-none"
                             placeholder="VerificationCode"
                             style={{ 'height': '75%', 'width': '100%', 'borderRadius': '5px' }}
                             ref={typedVerificationCode}
                         />
                     </Box>
                     <Box style={{ 'height': '15%', 'width': '77%', display: 'flex', justifyContent: 'center', alignItems: 'center', alignSelf: 'center' }}>
-                        <Button style={{ 'height': '75%', 'width': '100%', 'borderRadius': '5px' }} size="3" variant='solid' onClick={handleVarification}>Verify Account</Button>
+                        <Button style={{ 'height': '75%', 'width': '100%', 'borderRadius': '5px', 'backgroundColor': '#120052' }} size="3" variant='solid' onClick={handleVarification}>Verify Account</Button>
                     </Box>
                 </Flex>
             </Box>
